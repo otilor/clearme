d4eb535c21fe34c8d7f4d9d92014163222a5247d
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-12-29 17:17:50 +0100

    Use PHPDoc block for routing.

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('dashboard', function () {
-    return view('dashboard');
-});
+/** @var \Illuminate\Routing\Router $router */
+$router->get('dashboard', fn()=> view('dashboard'));
