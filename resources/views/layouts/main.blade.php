640e90aa7104c2a225b0c2d7bd3c27648a1ead8b
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-12-31 09:54:30 +0100

    Remove navlink

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -117,57 +117,6 @@
                         </div>
                     </a>
                 </li>
-
-                <li class="menu">
-                    <a href="#submenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
-                        <div class="">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-airplay"><path d="M5 17H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-1"></path><polygon points="12 15 17 21 7 21 12 15"></polygon></svg>
-                            <span> Menu 2</span>
-                        </div>
-                        <div>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg>
-                        </div>
-                    </a>
-                    <ul class="collapse submenu list-unstyled" id="submenu" data-parent="#accordionExample">
-                        <li>
-                            <a href="javascript:void(0);"> Submenu 1 </a>
-                        </li>
-                        <li>
-                            <a href="javascript:void(0);"> Submenu 2 </a>
-                        </li>
-                    </ul>
-                </li>
-
-                <li class="menu">
-                    <a href="#submenu2" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
-                        <div class="">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file"><path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path><polyline points="13 2 13 9 20 9"></polyline></svg>
-                            <span> Menu 3</span>
-                        </div>
-                        <div>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg>
-                        </div>
-                    </a>
-                    <ul class="collapse submenu list-unstyled" id="submenu2" data-parent="#accordionExample">
-                        <li>
-                            <a href="javascript:void(0);"> Submenu 1 </a>
-                        </li>
-                        <li>
-                            <a href="#sm2" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"> Submenu 2 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg> </a>
-                            <ul class="collapse list-unstyled sub-submenu" id="sm2" data-parent="#submenu2">
-                                <li>
-                                    <a href="javascript:void(0);"> Sub-Submenu 1 </a>
-                                </li>
-                                <li>
-                                    <a href="javascript:void(0);"> Sub-Submenu 2 </a>
-                                </li>
-                                <li>
-                                    <a href="javascript:void(0);"> Sub-Submenu 3 </a>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
             </ul>
 
         </nav>
