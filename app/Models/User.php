59a8d739e902fe7014a30a91a7e06f1f8e4bf2c4
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-01-01 15:26:30 +0100

    Create a new mode for users' profile

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected function profile()
+    {
+        return $this->hasOne(Profile::class);
+    }
 }
