be429b98ed7c46523e5f15573b8b33b5ab11ab1c
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-01-01 14:46:05 +0100

    Use GetsOnboarded trait in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Calebporzio\Onboard\GetsOnboarded;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -10,7 +11,7 @@ use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable, HasRoles;
+    use HasFactory, Notifiable, HasRoles, GetsOnboarded;
 
     /**
      * The attributes that are mass assignable.
