650903fbaf1cb7c1017e7d487412d888e46700cb
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-01-02 09:16:19 +0100

    Add Request class for save of profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -5,11 +5,17 @@ namespace App\Http\Controllers;
 
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreProfileRequest;
 
 class ProfileController extends Controller
 {
     public function index()
     {
         return view('profile');
+    }
+
+    public function store(StoreProfileRequest $request)
+    {
+        //
     }
 }
