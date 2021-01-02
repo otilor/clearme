f9fc7bf2d0c9bb0f19ee30ed3c56364df8e4422a
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-01-02 09:13:48 +0100

    Made page title more accessible.

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
-    <title>@yield('pageTitle') - ClearMe</title>
+    <title>@yield('pageTitle') | ClearMe</title>
     <link rel="icon" type="image/x-icon" href="{{ asset('img/favicon.ico') }}"/>
     <link href="{{ asset('assets/css/loader.css') }}" rel="stylesheet" type="text/css" />
     <script src="{{ asset('assets/js/loader.js')  }}"></script>
