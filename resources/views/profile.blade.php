cd82ba39b5d7855fa652b4c2edc9f34f1fcea890
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-01-02 18:16:00 +0100

    🚀🚀 Feat: display departments for all faculties dynamically.

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -23,12 +23,12 @@
                         <select name= "faculty" class="form-control" id="faculty" required>
                             <option selected="true" disabled="disabled">Choose Your faculty</option>
                             <option>Science and Science Education</option>
-                            <option>Humanities</option>
                             <option>Social and Management Sciences</option>
+                            <option>Humanities</option>
                         </select>
                     </div>
 
-                    <div class="form-group mb-4" id="science_departments" style="display: none">
+                    <div class="form-group mb-4 departments" id="science_departments" style="display: none">
                         <label for="exampleFormControlSelect1">Select your department</label>
                         <select name= "faculty" class="form-control" id="exampleFormControlSelect1" required>
                             <option selected="true" disabled="disabled">Choose Your department</option>
@@ -37,6 +37,31 @@
                             <option>Chemical Sciences</option>
                         </select>
                     </div>
+
+                    <div class="form-group mb-4 departments" id="social_science_departments" style="display: none">
+                        <label for="exampleFormControlSelect1">Select your department</label>
+                        <select name= "faculty" class="form-control" id="exampleFormControlSelect1" required>
+                            <option selected="true" disabled="disabled">Choose Your department</option>
+                            <option>Political Science and International Relations</option>
+                            <option>Economics</option>
+                            <option>Accounting</option>
+                            <option>Business Administration</option>
+                            <option>Mass Communication</option>
+                        </select>
+                    </div>
+
+                    <div class="form-group mb-4 departments" id="humanities_departments" style="display: none">
+                        <label for="exampleFormControlSelect1">Select your department</label>
+                        <select name= "faculty" class="form-control" id="exampleFormControlSelect1" required>
+                            <option selected="true" disabled="disabled">Choose Your department</option>
+                            <option>English and Literary studies</option>
+                            <option>History and diplomatic studies</option>
+                            <option>French</option>
+                            <option>Christian Religious Studies</option>
+                        </select>
+                    </div>
+
+
                     <div>
                         <button class="btn btn-primary" type="submit">Save <span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-save"><path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z"></path><polyline points="17 21 17 13 7 13 7 21"></polyline><polyline points="7 3 7 8 15 8"></polyline></svg></span></button>
                     </div>
@@ -49,11 +74,18 @@
         src="{{ asset('assets/js/jquery-3.5.1.slim.min.js') }}"></script>
     <script>
         $('#faculty').on('change',function(){
+            $(".departments").hide();
             var selection = $(this).val();
             switch(selection){
                 case "Science and Science Education":
                     $("#science_departments").show()
                     break;
+                case "Humanities":
+                    $("#humanities_departments").show()
+                    break;
+
+                case "Social and Management Sciences":
+                    $("#social_science_departments").show()
                 default:
                     $("#science_departments").hide()
             }
