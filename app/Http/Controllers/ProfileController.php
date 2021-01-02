ecb0eb6f500eb414e6824d7d7971b7c13c88ab5b
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-01-02 09:45:35 +0100

    Successfully update profile information.

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -6,9 +6,18 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreProfileRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
 {
+    protected User $user;
+
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
     public function index()
     {
         return view('profile');
@@ -16,6 +25,7 @@ class ProfileController extends Controller
 
     public function store(StoreProfileRequest $request)
     {
-        //
+        $this->user->find(Auth::id())->profile->update($request->all());
+        return "Done!";
     }
 }
