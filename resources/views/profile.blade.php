c91c7b5dc275f72ab30abef217ea3a8f3abe9999
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-01-08 09:03:45 +0100

    Add gender select field to profile.blade.php

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -66,6 +66,15 @@
                        <input name="phone_number" class="form-control" type="number">
                     </div>
 
+                    <div class="form-group mb-4" id="humanities_departments">
+                        <label for="exampleFormControlSelect1">Select your gender</label>
+                        <select name= "department" class="form-control" id="exampleFormControlSelect1" required>
+                            <option selected="true" disabled="disabled">Choose Your gender</option>
+                            <option>Male</option>
+                            <option>Female</option>
+                        </select>
+                    </div>
+
                     <div>
                         <button class="btn btn-primary" type="submit">Save <span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-save"><path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z"></path><polyline points="17 21 17 13 7 13 7 21"></polyline><polyline points="7 3 7 8 15 8"></polyline></svg></span></button>
                     </div>
