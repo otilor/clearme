2e7bd4dcd3605da4ced15d17b278bfe67ec5c5e2
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-01-09 16:44:24 +0100

    Attach auth middleware

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return redirect('/dashboard');
-});
+})->middleware(['auth']);
 
 /** @var \Illuminate\Routing\Router $router */
 
