aa2f1fa2f6bcf59b73c813d37388944aee84947a
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-01-13 23:37:26 +0100

    Attach middleware to dashboard route

--- routes/web.php
+++ routes/web.php
@@ -28,8 +28,8 @@ $router->group(['prefix' => 'student', 'middleware' => ['auth', 'role:student']]
 });
 
 $router->group(['prefix' => 'admin', 'middleware' => ['auth', 'role:admin']], function() use ($router){
-    $router->get('dashboard', [\App\Http\Controllers\Admin\AdminController::class, 'dashboard'])->name('admin.dashboard');
-    $router->get('complete-onboarding', [CompleteOnboarding::class,'index']);
+    $router->get('dashboard', [\App\Http\Controllers\Admin\AdminController::class, 'dashboard'])->name('admin.dashboard')->middleware('has_admin_completed_onboarding');
+    $router->get('complete-onboarding', [CompleteOnboarding::class,'index'])->name('admin.complete-onboarding');
 });
 
 $router->get('profile', [ProfileController::class, 'index'])->middleware('auth');
