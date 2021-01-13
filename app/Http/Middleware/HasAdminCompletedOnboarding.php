6b658729f85cea1c091b41c13ab271f2cbf08830
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-01-13 15:52:39 +0100

    Feat: Implement middleware for admin onboarding✌️

---
+++ app/Http/Middleware/HasAdminCompletedOnboarding.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\AdminsOnboarded;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HasAdminCompletedOnboarding
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (! Auth::check()) {
+            return redirect(route('login'));
+        }
+
+        if (! Auth::user()->hasRole('admin')) {
+            abort(403);
+        }
+
+
+        if (! AdminsOnboarded::find(Auth::id())) {
+            return response('Yikes, you have to complete your registration, bruv.');
+        }
+        return $next($request);
+    }
+}
