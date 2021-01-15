bea8cdd631cfa37a23c32c2144827a6c07330631
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-01-15 22:52:31 +0100

    Add new model with seeder and factory

---
+++ database/seeders/SectionSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Section;
+use Illuminate\Database\Seeder;
+
+class SectionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Section::factory()->times(1)->create();
+    }
+}
