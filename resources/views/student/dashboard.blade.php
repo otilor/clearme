dffdaaf965313f6d80fb7e0c6cae8ef7f75bbbb7
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-01-24 23:48:30 +0100

    wip

--- resources/views/student/dashboard.blade.php
+++ resources/views/student/dashboard.blade.php
@@ -13,7 +13,92 @@
 {{--                --}}
 {{--            </form>--}}
 
+            <div class="widget widget-table-one">
+                <div class="widget-heading">
+                    <h5 class="">Transactions</h5>
+                </div>
 
+                <div class="widget-content">
+                    <div class="transactions-list">
+                        <div class="t-item">
+                            <div class="t-company-name">
+                                <div class="t-icon">
+                                    <div class="icon">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
+                                    </div>
+                                </div>
+                                <div class="t-name">
+                                    <h4>Electricity Bill</h4>
+                                    <p class="meta-date">4 Aug 1:00PM</p>
+                                </div>
+
+                            </div>
+                            <div class="t-rate rate-dec">
+                                <p><span>-$16.44</span> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-down"><line x1="12" y1="5" x2="12" y2="19"></line><polyline points="19 12 12 19 5 12"></polyline></svg></p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="transactions-list">
+                        <div class="t-item">
+                            <div class="t-company-name">
+                                <div class="t-icon">
+                                    <div class="avatar avatar-xl">
+                                        <span class="avatar-title rounded-circle">SP</span>
+                                    </div>
+                                </div>
+                                <div class="t-name">
+                                    <h4>Shaun Park</h4>
+                                    <p class="meta-date">4 Aug 1:00PM</p>
+                                </div>
+                            </div>
+                            <div class="t-rate rate-inc">
+                                <p><span>+$66.44</span> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-up"><line x1="12" y1="19" x2="12" y2="5"></line><polyline points="5 12 12 5 19 12"></polyline></svg></p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="transactions-list">
+                        <div class="t-item">
+                            <div class="t-company-name">
+                                <div class="t-icon">
+                                    <div class="avatar avatar-xl">
+                                        <span class="avatar-title rounded-circle">AD</span>
+                                    </div>
+                                </div>
+                                <div class="t-name">
+                                    <h4>Amy Diaz</h4>
+                                    <p class="meta-date">4 Aug 1:00PM</p>
+                                </div>
+
+                            </div>
+                            <div class="t-rate rate-inc">
+                                <p><span>+$66.44</span> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-up"><line x1="12" y1="19" x2="12" y2="5"></line><polyline points="5 12 12 5 19 12"></polyline></svg></p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="transactions-list">
+                        <div class="t-item">
+                            <div class="t-company-name">
+                                <div class="t-icon">
+                                    <div class="icon">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
+                                    </div>
+                                </div>
+                                <div class="t-name">
+                                    <h4>Netflix</h4>
+                                    <p class="meta-date">4 Aug 1:00PM</p>
+                                </div>
+
+                            </div>
+                            <div class="t-rate rate-dec">
+                                <p><span>-$32.00</span> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-down"><line x1="12" y1="5" x2="12" y2="19"></line><polyline points="19 12 12 19 5 12"></polyline></svg></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
 {{--    <div id="timelineBasic" class="col-lg-12 layout-spacing">--}}
 {{--        <div class="statbox widget box box-shadow">--}}
