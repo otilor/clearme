c2ba0c5fec33ddc9ee6b48e86718f5bef4274509
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-01-24 23:23:59 +0100

    wip

--- resources/views/student/dashboard.blade.php
+++ resources/views/student/dashboard.blade.php
@@ -7,7 +7,7 @@
     <link href="{{ asset('assets/css/components/timeline/custom-timeline.css') }}" rel="stylesheet" type="text/css" />
     @if(! auth()->user()->onboarding()->inProgress())
         <div class="mx-auto text-center col-lg-12 layout-spacing">
-            <form action="/clearance/start" method="post">
+            <form action="clearance/start" method="post">
                 @csrf
                 <input class="btn btn-lg btn-primary" type="submit" value="Begin clearance">
             </form>
