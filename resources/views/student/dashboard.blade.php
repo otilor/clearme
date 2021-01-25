dd981ad00483a7a4414e9ef3a681d88f8ef27c71
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-01-25 16:22:45 +0100

    wip

--- resources/views/student/dashboard.blade.php
+++ resources/views/student/dashboard.blade.php
@@ -17,22 +17,22 @@
                 <div class="widget-heading">
                     <h5 class="">Sections to request clearance</h5>
                 </div>
-
+                
                 <div class="widget-content">
                     <div class="transactions-list">
                         <div class="t-item">
                             <div class="t-company-name">
-                                <div class="t-icon">
-                                    <div class="icon">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                             stroke-linecap="round" stroke-linejoin="round"
-                                             class="feather feather-home">
-                                            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
-                                            <polyline points="9 22 9 12 15 12 15 22"></polyline>
-                                        </svg>
-                                    </div>
-                                </div>
+{{--                                <div class="t-icon">--}}
+{{--                                    <div class="icon">--}}
+{{--                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"--}}
+{{--                                             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"--}}
+{{--                                             stroke-linecap="round" stroke-linejoin="round"--}}
+{{--                                             class="feather feather-home">--}}
+{{--                                            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>--}}
+{{--                                            <polyline points="9 22 9 12 15 12 15 22"></polyline>--}}
+{{--                                        </svg>--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
                                 <div class="t-name">
                                     <h4>Student Affairs</h4>
                                     <p class="meta-date">4 Aug 1:00PM</p>
@@ -49,17 +49,17 @@
                     <div class="transactions-list">
                         <div class="t-item">
                             <div class="t-company-name">
-                                <div class="t-icon">
-                                    <div class="icon">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                             stroke-linecap="round" stroke-linejoin="round"
-                                             class="feather feather-home">
-                                            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
-                                            <polyline points="9 22 9 12 15 12 15 22"></polyline>
-                                        </svg>
-                                    </div>
-                                </div>
+{{--                                <div class="t-icon">--}}
+{{--                                    <div class="icon">--}}
+{{--                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"--}}
+{{--                                             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"--}}
+{{--                                             stroke-linecap="round" stroke-linejoin="round"--}}
+{{--                                             class="feather feather-home">--}}
+{{--                                            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>--}}
+{{--                                            <polyline points="9 22 9 12 15 12 15 22"></polyline>--}}
+{{--                                        </svg>--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
                                 <div class="t-name">
                                     <h4>ICT Unit</h4>
                                     <p class="meta-date">4 Aug 1:00PM</p>
@@ -76,17 +76,17 @@
                     <div class="transactions-list">
                         <div class="t-item">
                             <div class="t-company-name">
-                                <div class="t-icon">
-                                    <div class="icon">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                             stroke-linecap="round" stroke-linejoin="round"
-                                             class="feather feather-home">
-                                            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
-                                            <polyline points="9 22 9 12 15 12 15 22"></polyline>
-                                        </svg>
-                                    </div>
-                                </div>
+{{--                                <div class="t-icon">--}}
+{{--                                    <div class="icon">--}}
+{{--                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"--}}
+{{--                                             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"--}}
+{{--                                             stroke-linecap="round" stroke-linejoin="round"--}}
+{{--                                             class="feather feather-home">--}}
+{{--                                            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>--}}
+{{--                                            <polyline points="9 22 9 12 15 12 15 22"></polyline>--}}
+{{--                                        </svg>--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
                                 <div class="t-name">
                                     <h4>Laboratories</h4>
                                     <p class="meta-date">4 Aug 1:00PM</p>
@@ -103,17 +103,17 @@
                     <div class="transactions-list">
                         <div class="t-item">
                             <div class="t-company-name">
-                                <div class="t-icon">
-                                    <div class="icon">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                             stroke-linecap="round" stroke-linejoin="round"
-                                             class="feather feather-home">
-                                            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
-                                            <polyline points="9 22 9 12 15 12 15 22"></polyline>
-                                        </svg>
-                                    </div>
-                                </div>
+{{--                                <div class="t-icon">--}}
+{{--                                    <div class="icon">--}}
+{{--                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"--}}
+{{--                                             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"--}}
+{{--                                             stroke-linecap="round" stroke-linejoin="round"--}}
+{{--                                             class="feather feather-home">--}}
+{{--                                            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>--}}
+{{--                                            <polyline points="9 22 9 12 15 12 15 22"></polyline>--}}
+{{--                                        </svg>--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
                                 <div class="t-name">
                                     <h4>Hall of Residence</h4>
                                     <p class="meta-date">4 Aug 1:00PM</p>
@@ -130,17 +130,17 @@
                     <div class="transactions-list">
                         <div class="t-item">
                             <div class="t-company-name">
-                                <div class="t-icon">
-                                    <div class="icon">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                             stroke-linecap="round" stroke-linejoin="round"
-                                             class="feather feather-home">
-                                            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
-                                            <polyline points="9 22 9 12 15 12 15 22"></polyline>
-                                        </svg>
-                                    </div>
-                                </div>
+{{--                                <div class="t-icon">--}}
+{{--                                    <div class="icon">--}}
+{{--                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"--}}
+{{--                                             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"--}}
+{{--                                             stroke-linecap="round" stroke-linejoin="round"--}}
+{{--                                             class="feather feather-home">--}}
+{{--                                            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>--}}
+{{--                                            <polyline points="9 22 9 12 15 12 15 22"></polyline>--}}
+{{--                                        </svg>--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
                                 <div class="t-name">
                                     <h4>Head of Department</h4>
                                     <p class="meta-date">4 Aug 1:00PM</p>
@@ -157,17 +157,17 @@
                     <div class="transactions-list">
                         <div class="t-item">
                             <div class="t-company-name">
-                                <div class="t-icon">
-                                    <div class="icon">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                             stroke-linecap="round" stroke-linejoin="round"
-                                             class="feather feather-home">
-                                            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
-                                            <polyline points="9 22 9 12 15 12 15 22"></polyline>
-                                        </svg>
-                                    </div>
-                                </div>
+{{--                                <div class="t-icon">--}}
+{{--                                    <div class="icon">--}}
+{{--                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"--}}
+{{--                                             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"--}}
+{{--                                             stroke-linecap="round" stroke-linejoin="round"--}}
+{{--                                             class="feather feather-home">--}}
+{{--                                            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>--}}
+{{--                                            <polyline points="9 22 9 12 15 12 15 22"></polyline>--}}
+{{--                                        </svg>--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
                                 <div class="t-name">
                                     <h4>Sports division</h4>
                                     <p class="meta-date">4 Aug 1:00PM</p>
@@ -184,17 +184,17 @@
                     <div class="transactions-list">
                         <div class="t-item">
                             <div class="t-company-name">
-                                <div class="t-icon">
-                                    <div class="icon">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                             stroke-linecap="round" stroke-linejoin="round"
-                                             class="feather feather-home">
-                                            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
-                                            <polyline points="9 22 9 12 15 12 15 22"></polyline>
-                                        </svg>
-                                    </div>
-                                </div>
+{{--                                <div class="t-icon">--}}
+{{--                                    <div class="icon">--}}
+{{--                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"--}}
+{{--                                             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"--}}
+{{--                                             stroke-linecap="round" stroke-linejoin="round"--}}
+{{--                                             class="feather feather-home">--}}
+{{--                                            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>--}}
+{{--                                            <polyline points="9 22 9 12 15 12 15 22"></polyline>--}}
+{{--                                        </svg>--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
                                 <div class="t-name">
                                     <h4>Security unit</h4>
                                     <p class="meta-date">4 Aug 1:00PM</p>
@@ -211,17 +211,17 @@
                     <div class="transactions-list">
                         <div class="t-item">
                             <div class="t-company-name">
-                                <div class="t-icon">
-                                    <div class="icon">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                             stroke-linecap="round" stroke-linejoin="round"
-                                             class="feather feather-home">
-                                            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
-                                            <polyline points="9 22 9 12 15 12 15 22"></polyline>
-                                        </svg>
-                                    </div>
-                                </div>
+{{--                                <div class="t-icon">--}}
+{{--                                    <div class="icon">--}}
+{{--                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"--}}
+{{--                                             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"--}}
+{{--                                             stroke-linecap="round" stroke-linejoin="round"--}}
+{{--                                             class="feather feather-home">--}}
+{{--                                            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>--}}
+{{--                                            <polyline points="9 22 9 12 15 12 15 22"></polyline>--}}
+{{--                                        </svg>--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
                                 <div class="t-name">
                                     <h4>Medicals</h4>
                                     <p class="meta-date">4 Aug 1:00PM</p>
@@ -238,17 +238,17 @@
                     <div class="transactions-list">
                         <div class="t-item">
                             <div class="t-company-name">
-                                <div class="t-icon">
-                                    <div class="icon">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                             stroke-linecap="round" stroke-linejoin="round"
-                                             class="feather feather-home">
-                                            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
-                                            <polyline points="9 22 9 12 15 12 15 22"></polyline>
-                                        </svg>
-                                    </div>
-                                </div>
+{{--                                <div class="t-icon">--}}
+{{--                                    <div class="icon">--}}
+{{--                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"--}}
+{{--                                             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"--}}
+{{--                                             stroke-linecap="round" stroke-linejoin="round"--}}
+{{--                                             class="feather feather-home">--}}
+{{--                                            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>--}}
+{{--                                            <polyline points="9 22 9 12 15 12 15 22"></polyline>--}}
+{{--                                        </svg>--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
                                 <div class="t-name">
                                     <h4>Bursary department</h4>
                                     <p class="meta-date">4 Aug 1:00PM</p>
