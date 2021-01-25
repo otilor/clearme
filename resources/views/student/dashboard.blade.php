2da6979a5da80273cfd2d40a27771600e7b68c14
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-01-25 16:09:03 +0100

    wip

--- resources/views/student/dashboard.blade.php
+++ resources/views/student/dashboard.blade.php
@@ -34,7 +34,10 @@
 
                             </div>
                             <div class="t-rate rate-dec">
-                                <p><span>-$16.44</span> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-down"><line x1="12" y1="5" x2="12" y2="19"></line><polyline points="19 12 12 19 5 12"></polyline></svg></p>
+                                <a class="btn btn-primary" href="/student/clearance/student-affairs">
+                                    @csrf
+                                    Request
+                                </a>
                             </div>
                         </div>
                     </div>
