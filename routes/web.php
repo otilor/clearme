71c9a0e3f5ab8cd8ade9f708e84a9f3cebb8c779
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-01-28 10:37:29 +0100

    wip

--- routes/web.php
+++ routes/web.php
@@ -27,60 +27,11 @@ $router->group(['prefix' => 'student', 'middleware' => ['auth', 'role:student']]
     $router->get('dashboard', [\App\Http\Controllers\Student\StudentController::class, 'dashboard'])->name('student.dashboard');
 });
 
-$router->group(['prefix' => 'clearance', 'middleware' => ['auth', 'role:student']], function () use ($router) {
-    $router->get('student-affairs', function () {
-        return "Student affairs!";
-    });
-    $router->get('ict-unit', function () {
-        return "ICT Unit!";
-    });
-    $router->get('laboratories', function () {
-        return "Laboratories";
-    });
-    $router->get('hall-of-residence', function () {
-        return "Hall of residence";
-    });
-    $router->get('head-of-department', function () {
-        return "Head of department";
-    });
-    $router->get('sports-division', function () {
-        return "Sports division";
-    });
-    $router->get('security-unit', function () {
-        return "Security unit";
-    });
-    $router->get('medicals', function () {
-        return "Medicals";
-    });
-    $router->get('bursary-department', function () {
-        return "Bursary department";
-    });
-});
-
 $router->group(['prefix' => 'admin', 'middleware' => ['auth', 'role:admin']], function () use ($router) {
-    $router->get('dashboard', [\App\Http\Controllers\Admin\AdminController::class, 'dashboard'])->name('admin.dashboard')->middleware('has_admin_completed_onboarding');
-    $router->get('complete-onboarding', [CompleteOnboardingController::class, 'index'])->name('admin.complete-onboarding');
-    $router->post('complete-onboarding', [CompleteOnboardingController::class, 'store']);
+    //
 });
 
-$router->get('profile', [ProfileController::class, 'index'])->middleware('auth');
-$router->post('profile', [ProfileController::class, 'store']);
-
-// Automatic redirect
-$router->get('dashboard', function () {
-    if (!Auth::check()) {
-        return redirect(\route('login'));
-    }
-
-    if (Auth::user()->hasRole('admin')) {
-        return redirect(\route('admin.dashboard'));
-    };
-
-    return redirect(\route('student.dashboard'));
-});
+// Automatic redirect;
 
 Auth::routes();
 
-$router->get('test', function () {
-    return new \App\Mail\NewAccountEmailVerificationMail(['ea']);
-});
