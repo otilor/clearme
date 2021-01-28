f5ca14b2dec3b970a8cd61d1c24741b2f73575c7
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-01-28 11:31:18 +0100

    Add new columns

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,9 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('matric_number')->unique()->nullable();
+            $table->unsignedBigInteger('department_id')->unique()->nullable();
+            $table->unsignedBigInteger('faculty_id')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
