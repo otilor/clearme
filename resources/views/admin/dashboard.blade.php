0108c99fb0565cd24ae9a0775427965d2d57d8ab
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-02-02 23:17:33 +0100

    wip

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -22,17 +22,6 @@
                     <div class="transactions-list">
                         <div class="t-item">
                             <div class="t-company-name">
-                                {{--                                <div class="t-icon">--}}
-                                {{--                                    <div class="icon">--}}
-                                {{--                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"--}}
-                                {{--                                             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"--}}
-                                {{--                                             stroke-linecap="round" stroke-linejoin="round"--}}
-                                {{--                                             class="feather feather-home">--}}
-                                {{--                                            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>--}}
-                                {{--                                            <polyline points="9 22 9 12 15 12 15 22"></polyline>--}}
-                                {{--                                        </svg>--}}
-                                {{--                                    </div>--}}
-                                {{--                                </div>--}}
                                 <div class="t-name">
                                     <h4>Student Affairs</h4>
                                     <p class="meta-date">4 Aug 1:00PM</p>
