7c20c0ede082272b1deb21cd858abcd7f869fbad
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-02-03 23:59:40 +0100

    wip

--- app/SetupApplication.php
+++ app/SetupApplication.php
@@ -6,5 +6,9 @@ namespace App;
 
 class SetupApplication
 {
-    //
+    public function __invoke()
+    {
+        // TODO: Convert everything in bash script to PHP interface, I think.
+        //
+    }
 }
