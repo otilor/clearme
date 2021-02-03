6580bb19332a4c3f3d361d9acc068e750450eb6a
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-02-03 12:20:22 +0100

    wip

--- app/Mail/SendAdminInviteMail.php
+++ app/Mail/SendAdminInviteMail.php
@@ -27,6 +27,6 @@ class SendAdminInviteMail extends Mailable
      */
     public function build()
     {
-        return $this->markdown('emails.invite-sectional-admin');
+        return $this->from(env('no-reply@clearme.test'))->markdown('emails.invite-sectional-admin');
     }
 }
