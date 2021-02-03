e4126e979ad10a035ae0d67d30fbbfccbda5abf9
Akinyosoye Gabriel <gabrielfemi799@gmail.com>
2021-02-03 19:29:32 +0100

    Revert "Fix bug on post-autoload-dump script on composer install"

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\InviteController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Student\StudentController;
 use App\Mail\SendAdminInviteMail;
+use App\Models\Section;
 use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -31,14 +32,24 @@ $router->group(['prefix' => 'student', 'middleware' => ['auth', 'role:student']]
     $router->get('dashboard', [StudentController::class, 'dashboard'])->name('student.dashboard');
 });
 
-$router->get('mailable', fn () => new SendAdminInviteMail());
+$router->group(['prefix' => 'admin', 'middleware' => ['auth', 'role:admin']], function () use ($router) {
+    $router->view('dashboard', 'admin.dashboard', ['sections' => Section::all()])->name('admin.dashboard');
+    $router->group(['prefix' => 'sections'], function () use ($router) {
+        $router->get('{section}/invite', [InviteController::class, 'invite']);
+        $router->post('{section}/invite', [InviteController::class, 'send']);
+    });
+});
+
+$router->get('mailable', fn() => new SendAdminInviteMail());
 
 $router->get('/dashboard', function () {
     if (Auth::user()->hasRole('admin')) {
         return redirect(\route('admin.dashboard'));
     }
-    echo "welcome  " . Auth::user()->roles;
+    echo "welcome  " . Auth::user()->roles ;
 })->middleware(['auth']);
 // Automatic redirect;
 
 Auth::routes(['register' => false]);
+
+
