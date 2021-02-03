f22fb52ad3d501c0f5c38f6aaf10b0937260350e
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-02-03 15:24:52 +0100

    wip

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -12,7 +12,7 @@
     <!-- BEGIN GLOBAL MANDATORY STYLES -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
     <link href="{{ asset('bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
-    @toastr_css
+    <x:notify-messages />
     <link href="{{ asset('assets/css/plugins.css')  }}" rel="stylesheet" type="text/css" />
     <!-- END GLOBAL MANDATORY STYLES -->
 
