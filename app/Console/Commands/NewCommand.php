d2163c06a2ef4f6e47002583a1608baa7bc643b3
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-02-04 01:04:25 +0100

    wip

--- app/Console/Commands/NewCommand.php
+++ app/Console/Commands/NewCommand.php
@@ -6,6 +6,7 @@ namespace App\Console\Commands;
 
 
 
+use Illuminate\Support\Facades\Artisan;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -21,6 +22,10 @@ class NewCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         // TODO: Set up authorization
+        $output->writeln('Setting up authorization 🔒🔒🔒');
+
+        // Clear permission cache
+        Artisan::call('permission:cache-reset');
 
         // TODO: Seed database
         return Command::SUCCESS;
