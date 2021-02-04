cf0a5a1cd319db3cae51597828973709fe5c6b09
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-02-04 15:22:20 +0100

    wip

--- app/Models/Section.php
+++ app/Models/Section.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Section extends Model
 {
     use HasFactory;
+
+    public function admin_invite()
+    {
+        return $this->hasOne(AdminInvite::class);
+    }
 }
