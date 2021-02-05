ce9420c4adb7ae8f920bfa4792e079bd09eaf685
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-02-05 16:34:53 +0100

    wip

--- tests/Feature/SetupCommandTest.php
+++ tests/Feature/SetupCommandTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class StartupCommandTest extends TestCase
+class SetupCommandTest extends TestCase
 {
     /**
      * A basic feature test example.
