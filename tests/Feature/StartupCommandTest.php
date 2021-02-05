b13c3708a60279331b98a6d9143558476148c9ce
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-02-05 16:34:14 +0100

    wip

---
+++ tests/Feature/StartupCommandTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class StartupCommandTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_startup_command_test()
+    {
+        //
+    }
+}
