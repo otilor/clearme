7992a57f1d35544bdb1a38a218cf501c044918b1
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-02-13 11:08:23 +0100

    Fix: duplicate entry for admin account

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class AdminSeeder extends Seeder
@@ -15,7 +16,9 @@ class AdminSeeder extends Seeder
      */
     public function run()
     {
-        $user = User::create(['name' => 'Administrator', 'email' => 'admin@clearme.test', 'password' => Hash::make('@dm1nt3st')]);
-        $user->assignRole('admin');
+        if (! User::where('email', 'admin@clearme.test')) {
+            $user = User::create(['name' => 'Administrator', 'email' => 'admin@clearme.test', 'password' => Hash::make('@dm1nt3st')]);
+            $user->assignRole('admin');
+        }
     }
 }
