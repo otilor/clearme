0f5268096e8331a579da05a6b0c2aa54bc47c300
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-02-13 22:07:24 +0100

    Add name to footer

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -140,7 +140,7 @@
                 <p class="">Copyright © {{ now()->year  }} <a rel="noopener noreferrer" target="_blank" href="https://github.com/GabrielFemi">Gabriel Akinyosoye</a>, All rights reserved.</p>
             </div>
             <div class="footer-section f-section-2">
-                <p class="">Coded with <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-heart"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg></p>
+                <p class="">Coded with <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-heart"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg> by Gabriel Akinyosoye</p>
             </div>
         </div>
     </div>
