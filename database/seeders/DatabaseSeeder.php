9078d6723eb6f8457e7aab085e846f81a6f35121
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-02-13 11:02:28 +0100

    Add SectionSeeder and AdminSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(RoleAndPermissionSeeder::class);
+        $this->call(AdminSeeder::class);
+        $this->call(SectionSeeder::class);
     }
 }
