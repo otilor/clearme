522102e3946dcd6d82e6fc77c94a49ac524d6bdd
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-02-14 05:55:40 +0100

    wip

--- app/Http/Controllers/InviteController.php
+++ app/Http/Controllers/InviteController.php
@@ -9,6 +9,7 @@ use App\Jobs\SendMail;
 use App\Models\AdminInvite;
 use App\Models\Section;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
 class InviteController extends Controller
@@ -20,27 +21,29 @@ class InviteController extends Controller
 
     public function send(SendAdminInviteMailRequest $request)
     {
+        DB::transaction(function () {
+            AdminInvite::create([
+                'section_id' => $section_id,
+                'status' => AdminInvite::PENDING
+            ]);
+
+            $user = User::create(['name' => 'Administrator', 'email' => $request->email, 'password' => bcrypt($unhashedPassword)]);
+
+            $section_id = $request->section_id;
+
+            $unhashedPassword = Str::random(8);
+
+            // Assign role to user
+            $user->assignRole('sectional_admin');
+
+            $data = (object)['user' => $user, 'unhashedPassword' => $unhashedPassword, 'section' => Section::find($section_id)];
+
+            dispatch(new SendMail($request->email, $data));
+
+            notify('success')->success("Contacted {$request->email} via mail");
+
+            return redirect(route('admin.dashboard'));
+        });
         // TODO: add tests
-        $section_id = $request->section_id;
-        AdminInvite::create([
-            'section_id' => $section_id,
-            'status' => AdminInvite::PENDING
-        ]);
-
-
-        $unhashedPassword = Str::random(8);
-
-        $user = User::create(['name' => 'Administrator', 'email' => $request->email, 'password' => bcrypt($unhashedPassword)]);
-
-        // Assign role to user
-        $user->assignRole('sectional_admin');
-
-        $data = (object)['user' => $user, 'unhashedPassword' => $unhashedPassword, 'section' => Section::find($section_id)];
-
-        dispatch(new SendMail($request->email, $data));
-
-        notify('success')->success("Contacted {$request->email} via mail");
-
-        return redirect(route('admin.dashboard'));
     }
 }
