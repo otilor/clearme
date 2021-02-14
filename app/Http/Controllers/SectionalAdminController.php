acd8ec150e2830cf81ad40ba8c3679ccb2ff4380
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-02-14 15:37:04 +0100

    Get students from the database

--- app/Http/Controllers/SectionalAdminController.php
+++ app/Http/Controllers/SectionalAdminController.php
@@ -4,10 +4,13 @@
 namespace App\Http\Controllers;
 
 
+use App\Models\User;
+
 class SectionalAdminController extends Controller
 {
     public function dashboard()
     {
-        return view('sectional_admin.dashboard');
+        $students = User::role('student')->get();
+        return view('sectional_admin.dashboard', compact('students'));
     }
 }
