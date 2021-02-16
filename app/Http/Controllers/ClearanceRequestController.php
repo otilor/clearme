e670f3a17d8856dae16c16a4e363e74866df64f3
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-02-16 13:21:51 +0100

    Get the clearance request first

--- app/Http/Controllers/ClearanceRequestController.php
+++ app/Http/Controllers/ClearanceRequestController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ClearanceRequest;
 use Illuminate\Http\Request;
 
 class ClearanceRequestController extends Controller
 {
     public function update(Request $request)
     {
-        return $request->all();
+        $clearanceRequest = ClearanceRequest::where('user_id', $request->student_id)->get();
+        return $clearanceRequest;
     }
 }
