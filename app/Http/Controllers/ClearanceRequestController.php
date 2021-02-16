41b0453851b6c673dc24f8d1f90d13a335698788
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-02-16 14:36:37 +0100

    Fix bug trying to get property on bool value

--- app/Http/Controllers/ClearanceRequestController.php
+++ app/Http/Controllers/ClearanceRequestController.php
@@ -10,8 +10,9 @@ class ClearanceRequestController extends Controller
     public function update(Request $request)
     {
         $clearanceRequest = ClearanceRequest::where('user_id', $request->student_id)
-            ->first()
-            ->update(['is_cleared' => true]);
+            ->first();
+
+        $clearanceRequest->update(['is_cleared' => true]);
 
         notify()->success("Student {$clearanceRequest->user?->name} has been cleared");
         return back();
@@ -19,8 +20,9 @@ class ClearanceRequestController extends Controller
     public function reject(Request $request)
     {
         $clearanceRequest = ClearanceRequest::where('user_id', $request->student_id)
-            ->first()
-            ->update(['is_cleared' => false]);
+            ->first();
+
+        $clearanceRequest->update(['is_cleared' => false]);
 
         notify()->error("Student has been rejected");
         return back();
