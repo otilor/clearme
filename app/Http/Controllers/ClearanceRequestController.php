876395a504797f99ad52d5e4668917c52b660540
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-02-16 14:04:46 +0100

    Made code more readable

--- app/Http/Controllers/ClearanceRequestController.php
+++ app/Http/Controllers/ClearanceRequestController.php
@@ -9,7 +9,10 @@ class ClearanceRequestController extends Controller
 {
     public function update(Request $request)
     {
-        $clearanceRequest = ClearanceRequest::where('user_id', $request->student_id)->first()->update(['is_cleared' => true]);
+        $clearanceRequest = ClearanceRequest::where('user_id', $request->student_id)
+            ->first()
+            ->update(['is_cleared' => true]);
+
         notify()->success("Student {$clearanceRequest->user?->name} has been cleared");
         return back();
     }
