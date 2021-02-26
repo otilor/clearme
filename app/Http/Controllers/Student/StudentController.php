a243ad52e1436cf39f13989d2e28da26d62a44d4
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-02-26 13:23:26 +0100

    wip

--- app/Http/Controllers/Student/StudentController.php
+++ app/Http/Controllers/Student/StudentController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Student;
 
 use App\Http\Controllers\Controller;
+use App\Models\ClearanceRequest;
+use App\Models\Section;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
     public function dashboard()
     {
-        return view('student.dashboard');
+        $clearanceRequest = ClearanceRequest::find(auth()->id());
+        return view('student.dashboard', $clearanceRequest);
     }
 }
