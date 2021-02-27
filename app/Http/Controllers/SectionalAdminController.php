c77370bcdb0bfcbe5a7ef8b16b1d5c94d4dd6eb3
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-02-27 10:11:10 +0100

    wip

--- app/Http/Controllers/SectionalAdminController.php
+++ app/Http/Controllers/SectionalAdminController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers;
 
 
 use App\Models\User;
+use Doctrine\Common\Cache\Cache;
 use Illuminate\Support\Arr;
 
 class SectionalAdminController extends Controller
 {
     public function dashboard()
     {
-        $students = User::notRole(['admin', 'sectional_admin'])->get();
+        $students = User::notRole(['admin', 'sectional_admin'])->paginate(20);
         return view('sectional_admin.dashboard', compact('students'));
     }
 }
