f7f71441fe8dfe160116e88abdaeaa919a4435b8
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-07-05 11:34:52 +0100

    Users are students by default.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,7 +3,10 @@
 namespace Database\Factories;
 
 use App\Models\User;
+use Closure;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
 use JetBrains\PhpStorm\ArrayShape;
 
@@ -30,5 +33,17 @@ class UserFactory extends Factory
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
+    }
+
+    public function configure()
+    {
+        return $this->afterMaking(static function (User $user) {
+           $user->assignRole('student');
+           tap(function () {
+              return $this->afterCreating(static function (User $user) {
+                  $user->assignRole('student');
+              });
+           });
+        });
     }
 }
