a9863f89a92f0ea5a2108857e5e6720203552657
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-07-05 11:38:50 +0100

    Add ClearanceRequestSeeder to Database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([RoleAndPermissionSeeder::class, AdminSeeder::class, SectionSeeder::class, StudentSeeder::class]);
+        $this->call([RoleAndPermissionSeeder::class, AdminSeeder::class, SectionSeeder::class, StudentSeeder::class, ClearanceRequestSeeder::class]);
     }
 }
