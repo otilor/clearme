64ccfb58f4fad3545a9b9d7e9fca77ff29857113
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-07-05 11:37:39 +0100

    Define factory properties

--- database/factories/ClearanceRequestFactory.php
+++ database/factories/ClearanceRequestFactory.php
@@ -24,10 +24,23 @@ class ClearanceRequestFactory extends Factory
     public function definition()
     {
         return [
-            'student_id' => $user = User::factory()->afterCreating(function ($user){
-                $user->assignRole('student');
-            }),
+            'student_id' => User::factory()->create(),
             'current_phase' => 3,
+             'payload' => [
+                 'status' =>
+                [
+                    'bursary' => ClearanceRequest::PENDING,
+                    'admin' => ClearanceRequest::PENDING,
+                    'student_affairs' => ClearanceRequest::PENDING,
+                    'laboratories' => ClearanceRequest::PENDING,
+                    'halls_of_residence' => ClearanceRequest::PENDING,
+                    'head_of_department' => ClearanceRequest::PENDING,
+                    'ict' => ClearanceRequest::PENDING,
+                    'security' => ClearanceRequest::PENDING,
+                    'medicals' => ClearanceRequest::PENDING,
+                    'sports' => ClearanceRequest::PENDING,
+                ]
+            ],
             'passed_phases' => json_encode([1, 2]),
             'other_phases' => json_encode([4, 5, 6, 7, 8])
         ];
