855f4664274c7ed83d4d7b25218bafcb2d063cc8
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-07-10 01:44:38 +0100

    wip

--- app/Http/Controllers/ClearanceRequestController.php
+++ app/Http/Controllers/ClearanceRequestController.php
@@ -7,18 +7,19 @@ use Illuminate\Http\Request;
 
 class ClearanceRequestController extends Controller
 {
-    public function __construct(public ClearanceRequest $clearanceRequest) {}
+    private string $slug;
+
+    public function __construct(public ClearanceRequest $clearanceRequest)
+    {
+        $this->slug = auth()->user()->mySection->slug;
+    }
 
     public function update(Request $request)
     {
-
-        $currentSection = auth()->user()->mySection->slug;
-
         $clearanceRequest = $this->clearanceRequest->where('student_id', $request->student_id)->first();
         $clonedClearanceRequest = clone $clearanceRequest;
 
-
-        $clearanceRequest->forceFill(["payload->status->{$currentSection}" => ClearanceRequest::APPROVED ])->save();
+        $clearanceRequest->forceFill(["payload->status->{$this->slug}" => ClearanceRequest::APPROVED ])->save();
 
         notify()->success("{$clonedClearanceRequest->student->name} has been cleared");
         return back();
