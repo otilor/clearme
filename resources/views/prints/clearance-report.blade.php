d416b9839474226461db689d08d136f0e563e041
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-07-31 18:18:28 +0100

    wip

--- resources/views/prints/clearance-report.blade.php
+++ resources/views/prints/clearance-report.blade.php
@@ -1,12 +1,33 @@
 <!DOCTYPE html>
 <head>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ public_path('css/app.css') }}" rel="stylesheet">
     <title>Clearance Report</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
 
 </head>
 
 <body>
 <!-- component -->
-
+<div class="modal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>Modal body text goes here.</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary">Save changes</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
 </body>
 </html>
