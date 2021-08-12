2ad76a6e9af2ec076b3b8c99e12e40c06e5f6f12
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-08-12 04:51:55 +0100

    Add academic affairs

--- database/seeders/SectionSeeder.php
+++ database/seeders/SectionSeeder.php
@@ -14,6 +14,7 @@ class SectionSeeder extends Seeder
     public function run()
     {
         $sections = [
+            'Academic Affairs',
             'Student Affairs',
             'Library',
             'ICT Unit',
